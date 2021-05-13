95a5243bfc5aa70748385ff631d8599c2ea07d48
Will Morris <teamexangus@gmail.com>
2021-05-13 14:05:45 +1000

    Add project files.

---
+++ BetterSpawnTickets/BetterSpawnTickets.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+using PlayerHandler = Exiled.Events.Handlers.Player;
+using ServerHandler = Exiled.Events.Handlers.Server;
+using MapHandler = Exiled.Events.Handlers.Map;
+using WarheadHandler = Exiled.Events.Handlers.Warhead;
+
+namespace BetterSpawnTickets
+{
+    public class BetterSpawnTickets : Plugin<Config>
+    {
+        //Plugin set-up stuff which I copied from a tutorial
+        private static readonly Lazy<BetterSpawnTickets> LazyInstance = new Lazy<BetterSpawnTickets>(valueFactory: () => new BetterSpawnTickets());
+
+        public static BetterSpawnTickets Instance => LazyInstance.Value;
+
+        //Plugin priority determines when during startup the plugin gets loaded
+        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+
+        //Event handlers
+        private Handlers.Player player;
+
+        private Handlers.Server server;
+
+        private Handlers.Map map;
+
+        //MTF can only be eliminated when this value is true - is set to true when MTF spawns.
+        public static bool HasMTFSpawned = Instance.Config.EliminateMtfBeforeFirstWave;
+
+        public static bool MtfEliminated = false;
+        public static bool ChaosEliminated = false;
+
+        //Copied from tutorial
+        private BetterSpawnTickets()
+        {
+        }
+
+        //Get the number of living players on Chaos' team
+        public static int CountFoundationForces()
+        {
+            int returnVal = 0;
+
+            foreach (Player player in Exiled.API.Features.Player.List)
+            {
+                //Increment returnVal for every player who is a scientist, guard, or NTF
+                if (new List<RoleType> { RoleType.Scientist, RoleType.FacilityGuard, RoleType.NtfCadet, RoleType.NtfLieutenant, RoleType.NtfScientist, RoleType.NtfCommander }.Contains(player.Role))
+                {
+                    returnVal++;
+                }
+            }
+
+            return returnVal;
+        }
+
+        public static void TeamsStayEliminatedLogic()
+        {
+            //TeamsStayEliminated logic
+            if (Respawn.NtfTickets == 0) { BetterSpawnTickets.MtfEliminated = false; }
+            if (BetterSpawnTickets.MtfEliminated && BetterSpawnTickets.Instance.Config.TeamsStayEliminated) { Respawn.GrantTickets(Respawning.SpawnableTeamType.NineTailedFox, -1 * Respawn.NtfTickets); }
+
+            if (Respawn.ChaosTickets == 0) { BetterSpawnTickets.ChaosEliminated = false; }
+            if (BetterSpawnTickets.ChaosEliminated && BetterSpawnTickets.Instance.Config.TeamsStayEliminated) { Respawn.GrantTickets(Respawning.SpawnableTeamType.ChaosInsurgency, -1 * Respawn.ChaosTickets); }
+        }
+
+        //Get the number of living players on the Foundation's team
+        public static int CountChaosForces()
+        {
+            int returnVal = 0;
+
+            foreach (Player player in Exiled.API.Features.Player.List)
+            {
+                //Increment returnVal for every player who is a chaos or class-d
+                if (new List<RoleType> { RoleType.ChaosInsurgency, RoleType.ClassD }.Contains(player.Role))
+                {
+                    returnVal++;
+                }
+            }
+
+            return returnVal;
+        }
+
+        //Run startup code when plugin is enabled
+        public override void OnEnabled()
+        {
+            RegisterEvents();
+        }
+
+        //Run shutdown code when plugin is disabled
+        public override void OnDisabled()
+        {
+            UnregisterEvents();
+        }
+
+        //Plugin startup code
+        public void RegisterEvents()
+        {
+            player = new Handlers.Player();
+            server = new Handlers.Server();
+            map = new Handlers.Map();
+
+            PlayerHandler.Dying += player.OnDying;
+            PlayerHandler.EscapingPocketDimension += player.EscapingPD;
+            ServerHandler.RespawningTeam += server.OnRespawnWave;
+            MapHandler.GeneratorActivated += map.OnGeneratorActivated;
+            WarheadHandler.Detonated += map.OnWarheadDetonation;
+        }
+
+        //Plugin shutdown code
+        public void UnregisterEvents()
+        {
+            PlayerHandler.Dying -= player.OnDying;
+            PlayerHandler.EscapingPocketDimension -= player.EscapingPD;
+            ServerHandler.RespawningTeam -= server.OnRespawnWave;
+            MapHandler.GeneratorActivated -= map.OnGeneratorActivated;
+            WarheadHandler.Detonated -= map.OnWarheadDetonation;
+
+            player = null;
+            server = null;
+            map = null;
+        }
+    }
+}
---
+++ BetterSpawnTickets/Config.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+using Exiled.API.Interfaces;
+
+namespace BetterSpawnTickets
+{
+    public sealed class Config : IConfig
+    {
+        [Description("Whether or not the plugin is enabled on this server.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Whether or not a team's tickets should be removed when all of its members are dead.")]
+        public bool EliminateTeams { get; set; } = true;
+
+        [Description("Prevents teams that have been eliminated from becoming un-eliminated.")]
+        public bool TeamsStayEliminated { get; set; } = true;
+
+        [Description("Whether or not MTF can be eliminated before the first spawn wave has occured. Its recommended to keep this as false.")]
+        public bool EliminateMtfBeforeFirstWave { get; set; } = false;
+
+        [Description("Whether or not a team's tickets should be transferred to the other team once the team dies.")]
+        public bool TransferTickets { get; set; } = true;
+
+        [Description("How many tickets the MTF team should be granted when a given class is killed by them. Supports Negative Values")]
+        public Dictionary<string, int> MtfTicketsOnKill { get; set; } = new Dictionary<string, int>
+        {
+            {"ClassD", 0 },
+            {"ChaosInsurgency", 0 },
+            {"FacilityGuard", 0 },
+            {"NtfCadet", 0 },
+            {"NtfLieutenant", 0 },
+            {"NtfScientist", 0 },
+            {"NtfCommander", 0 },
+            {"Scientist", 0 },
+            {"Scp049", 0 },
+            {"Scp0492", 0 },
+            {"Scp079", 0 },
+            {"Scp096", 0 },
+            {"Scp106", 0 },
+            {"Scp173", 0 },
+            {"Scp93953", 0 },
+            {"Scp93989", 0 },
+            {"Tutorial", 0 },
+        };
+
+        [Description("How many tickets the Chaos team should be granted when a given class is killed by them. Supports negative values.")]
+        public Dictionary<string, int> ChaosTicketsOnKill { get; set; } = new Dictionary<string, int>
+        {
+            {"ClassD", 0 },
+            {"ChaosInsurgency", 0 },
+            {"FacilityGuard", 0 },
+            {"NtfCadet", 0 },
+            {"NtfLieutenant", 0 },
+            {"NtfScientist", 0 },
+            {"NtfCommander", 0 },
+            {"Scientist", 0 },
+            {"Scp049", 0 },
+            {"Scp0492", 0 },
+            {"Scp079", 0 },
+            {"Scp096", 0 },
+            {"Scp106", 0 },
+            {"Scp173", 0 },
+            {"Scp93953", 0 },
+            {"Scp93989", 0 },
+            {"Tutorial", 0 },
+        };
+
+        [Description("How many tickets the MTF team should be granted for a given event. Supports negative values.")]
+        public Dictionary<string, int> MtfTicketsOnEvent { get; set; } = new Dictionary<string, int>
+        {
+            {"GeneratorActivated", 0 },
+            {"WarheadDetonation", 0 },
+            {"PocketdimensionEscape", 0 },
+            {"MtfRespawn", 0 },
+            {"ChaosRespawn", 0 }
+        };
+
+        [Description("How many tickets the Chaos team should be granted for a given event. Supports negative values.")]
+        public Dictionary<string, int> ChaosTicketsOnEvent { get; set; } = new Dictionary<string, int>
+        {
+            {"GeneratorActivated", 0 },
+            {"WarheadDetonation", 0 },
+            {"PocketdimensionEscape", 0 },
+            {"MtfRespawn", 0 },
+            {"ChaosRespawn", 0 }
+        };
+    }
+}
