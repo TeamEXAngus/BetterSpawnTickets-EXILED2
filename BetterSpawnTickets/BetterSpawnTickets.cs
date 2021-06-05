f8671608299a1afb22fe194649fed442b7e6633f
Will Morris <teamexangus@gmail.com>
2021-06-05 19:49:35 +1000

    Fixed version number

--- BetterSpawnTickets/BetterSpawnTickets.cs
+++ BetterSpawnTickets/BetterSpawnTickets.cs
@@ -16,7 +16,7 @@ namespace BetterSpawnTickets
         public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
         public override Version RequiredExiledVersion { get; } = new Version(2, 10, 0);
-        public override Version Version { get; } = new Version(1, 0, 1);
+        public override Version Version { get; } = new Version(1, 0, 2);
 
         //Declaring event handlers
 
